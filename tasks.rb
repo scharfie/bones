b760a1e9c1ebee51f5489c6268fe5d251fc1a5c6
Chris Scharf <scharfie@gmail.com>
2008-08-20 18:37:30 -0400

    Added Rakefile

--- tasks.rb
+++ tasks.rb
@@ -1,18 +1,18 @@
 desc "Start bones server"
 task :server do
   ARGV.shift
-  load File.join(File.dirname(__FILE__), 'bones/server.rb')
+  load File.join(File.dirname(__FILE__), 'server.rb')
 end
 
 desc "Cache page templates for redistribution"
 task :cache do
   ARGV.shift
-  load File.join(File.dirname(__FILE__), 'bones/cache.rb')
+  load File.join(File.dirname(__FILE__), 'cache.rb')
 end
 
 task :clean do
   # TODO
   # ARGV.shift
   # ARGV.unshift '--clean'
-  # load File.join(File.dirname(__FILE__), 'bones/cache.rb')
+  # load File.join(File.dirname(__FILE__), 'cache.rb')
 end
